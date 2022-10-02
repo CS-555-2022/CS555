d0e6f3513a9fcaa9c221da754cb860b7a4e5c12f
dzwfighting <yiwen7943@gmail.com>
2022-10-02 03:01:51 -0400

    Update User story - 30, 31

---
+++ US30.py
@@ -0,0 +1,20 @@
+# Yiwen Wang
+
+from US30_31_file import *
+import sys
+from unittest.mock import patch
+from io import StringIO
+# print(sys.argv)
+file_name = sys.argv[1]
+info = get_info(file_name)
+
+def list_married():
+    for fam in info['families']:	
+        if fam['married'] != None:
+            print(" Married people (husband, wife): " + str(fam['husband_name']) + str(fam['wife_name']))
+    return 0
+
+
+
+if __name__ == "__main__":
+    list_married()
